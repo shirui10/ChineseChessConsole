ce16579dc8081a750d62f22053b738be350011aa
shirui10 <240816440@qq.com>
2020-11-19 12:13:34 +0800

    修改1119-2

--- XIANGQI/Control/PieceControl/Horse.cs
+++ XIANGQI/Control/PieceControl/Horse.cs
@@ -10,14 +10,14 @@ namespace Control
 
             if (Math.Abs(chozenX / 2 - X / 2) == 1 && Math.Abs(chozenY - Y) == 4)           //横着走日字
             {
-                if (Matrix[chozenX, chozenY + 2 * (Y - chozenY) / Math.Abs(Y - chozenY)].side != Chess.Player.blank)        //避免斩马腿
+                if (Matrix[chozenX, chozenY + 2 * (Y - chozenY) / Math.Abs(Y - chozenY)].side != Chess.Player.blank)        //斩马腿
                 {
                     return false;
                 }
             }
             else if (Math.Abs(chozenX / 2 - X / 2) == 2 && Math.Abs(chozenY - Y) == 2)      //竖着走日字
             {
-                if (Matrix[chozenX + (X - chozenX) / Math.Abs(X - chozenX) * 2, chozenY].side != Chess.Player.blank)        //避免斩马腿
+                if (Matrix[chozenX + (X - chozenX) / Math.Abs(X - chozenX) * 2, chozenY].side != Chess.Player.blank)        //斩马腿
                 {
                     return false;
                 }
